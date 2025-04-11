0ec628271b13fadc65000df02b1e756bd9e41ec9
Romaric BANGA <romaric.banga@aninf.ga>
2025-04-11 11:29:24 +0100

    Structure initial du projet

--- src/main/java/ga/banga/calculatriceversion/CalculatriceversionApplication.java
+++ src/main/java/ga/banga/calculatriceversion/CalculatriceversionApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatriceversionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatriceversionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatriceversionApplication.class, args);
+    }
 
 }
--- src/test/java/ga/banga/calculatriceversion/CalculatriceversionApplicationTests.java
+++ src/test/java/ga/banga/calculatriceversion/CalculatriceversionApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CalculatriceversionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
