4cbba9e92305783d2090f3488f622d3453a06696
Romaric BANGA <romaric.banga@aninf.ga>
2025-04-11 11:19:12 +0100

    Initial commit

---
+++ src/main/java/ga/banga/calculatriceversion/CalculatriceversionApplication.java
@@ -0,0 +1,13 @@
+package ga.banga.calculatriceversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatriceversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatriceversionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ga/banga/calculatriceversion/CalculatriceversionApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.banga.calculatriceversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatriceversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
